6e2f9e9be6d2fb34b10e940a5b72f7730e55d3e4
Genaro Cancino <g.cancinohqgmail.com>
2025-11-27 02:56:43 -0600

    Initial Commit

--- app/src/main/java/com/gcancino/levelingup/ui/components/topBars/DashboardTopBar.kt
+++ app/src/main/java/com/gcancino/levelingup/ui/components/topBars/DashboardTopBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.gcancino.levelingup.R
 import com.gcancino.levelingup.core.Container
@@ -20,7 +21,6 @@ import com.gcancino.levelingup.ui.components.QuestDropDownMenu
 @Composable
 fun DashboardTopBar(
     expandDropDown: Boolean,
-    appContainer: Container,
     navController: NavHostController
 ) {
     CenterAlignedTopAppBar(
@@ -43,7 +43,7 @@ fun DashboardTopBar(
             QuestDropDownMenu(
                 expanded = expandDropDown,
                 onDismissRequest = { expandDropDown },
-                viewModel =  appContainer.questMenuViewModel,
+                viewModel = hiltViewModel(),
                 navController = navController
             )
         }
