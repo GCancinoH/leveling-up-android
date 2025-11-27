b919fd4a46f2bdcec2482b1d921a3af5a41cabd0
Genaro Cancino <g.cancinohqgmail.com>
2025-11-27 03:00:41 -0600

    Initial Commit

--- app/src/main/java/com/gcancino/levelingup/MainActivity.kt
+++ app/src/main/java/com/gcancino/levelingup/MainActivity.kt
@@ -6,41 +6,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gcancino.levelingup.core.Navigation
-import com.gcancino.levelingup.core.VoiceToTextParser
 import com.gcancino.levelingup.ui.theme.LevelingUpTheme
 import com.google.firebase.FirebaseApp
+import dagger.hilt.android.AndroidEntryPoint
 import kotlin.getValue
 
+@AndroidEntryPoint
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
+
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val context = applicationContext
-
-        // Initialize Firebase
-        FirebaseApp.initializeApp(context)
-
         // Setting app orientation
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-
-        // Voice Parser
-        val voiceToTextParser by lazy { VoiceToTextParser(application) }
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         enableEdgeToEdge()
         setContent {
             LevelingUpTheme {
-                Navigation(context)
+                Navigation()
             }
         }
     }
